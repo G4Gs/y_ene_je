16357366b40bea7105cdec30ddf9fcaab630fcee
GregoEl+k <106344297+G4Gs@users.noreply.github.com>
2025-07-17 08:48:08 -0300

    Add files via upload

---
+++ ing2proyecto-2025- el que me borra es gay/demo/src/main/java/com/sistemaing2/demo/ConfucionApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaing2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfucionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfucionApplication.class, args);
+	}
+
+}
